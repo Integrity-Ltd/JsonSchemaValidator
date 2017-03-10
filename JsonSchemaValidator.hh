ebfc3668800885fcff9b4f6e52262fa42c78ec08
Tibor Dravecz <t.gthb+17@integrity.hu>
2017-03-10 10:27:21 +0100

    Update JsonSchemaValidator.hh

--- JsonSchemaValidator.hh
+++ JsonSchemaValidator.hh
@@ -6,6 +6,7 @@
 #include <stdexcept>
 
 #include <jsoncpp/json/json.h>
+
 struct Reference {
 private:
 	const Json::Value * _ptr;
@@ -13,9 +14,12 @@ public:
 	std::string _position;
 	const Json::Value & _value;
 	explicit Reference(const Json::Value &);
+	explicit Reference(const Reference &);
 	explicit Reference(const Json::Value &, const std::string &);
 	explicit Reference(const Reference & r, const std::string &);
 	explicit Reference(const Reference & r, const int &);
+	void remap(const Reference & r);
+	void forward(const std::string &pos);
 };
 
 
@@ -27,13 +31,13 @@ private:
 	const Json::Value &_root, &_rootSchema;
 	static std::map<const std::string, const std::string> _format;
 	std::stringstream & error();
-	Reference getReferece(const Json::Value &, const std::string &, const std::string &);
+	Reference getReferece(const Json::Value &, const std::string &);
 	void validateArray(const Reference &, const Json::Value &);
 	void validateObject(const Reference &, const Json::Value &);
 	void validateString(const Reference &, const Json::Value &);
 	void validateNumber(const Reference &, const Json::Value &);
 	void validateInteger(const Reference &, const Json::Value &);
-	void validateBoolean(const Reference &, const Json::Value &);
+	void validateBoolean(const Reference &);
 	void validateEnum(const Reference &, const Json::Value &);
 	bool validateRegex(const std::string &, const std::string &);
 	bool validateAllOf(const Reference &, const Json::Value &);
@@ -43,7 +47,6 @@ private:
 	void validate(const Reference &, const Json::Value &, bool = true);
 	JsonSchemaValidator(const Json::Value &, const Reference &, const Json::Value &schema, bool = false);
 public:
-	static void init();
 	JsonSchemaValidator(const Json::Value &, const Json::Value &, bool = false);
 	bool getResult() const;
 	std::string getError() const;
