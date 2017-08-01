f9af46acee0cf4568b4f258d2afb28d3f4e72230
Tibor Dravecz <t.gthb+17@integrity.hu>
2017-08-01 16:16:41 +0200

    Update JsonSchemaValidator.hh

--- JsonSchemaValidator.hh
+++ JsonSchemaValidator.hh
@@ -1,5 +1,5 @@
-#ifndef JSONSCHEMAVALIDATOR_HH_
-#define JSONSCHEMAVALIDATOR_HH_
+#ifndef JSON_SCHEMA_VALIDATOR_HH_
+#define JSON_SCHEMA_VALIDATOR_HH_
 
 #include <string>
 #include <sstream>
@@ -7,49 +7,54 @@
 
 #include <jsoncpp/json/json.h>
 
-struct Reference {
-private:
-	const Json::Value * _ptr;
+class Json_Schema_Validator {
 public:
-	std::string _position;
-	const Json::Value & _value;
-	explicit Reference(const Json::Value &);
-	explicit Reference(const Reference &);
-	explicit Reference(const Json::Value &, const std::string &);
-	explicit Reference(const Reference & r, const std::string &);
-	explicit Reference(const Reference & r, const int &);
-	void remap(const Reference & r);
-	void forward(const std::string &pos);
+	typedef Json::Value JSON;
+private:
+	std::stringstream sserror_;
+	bool result_;
+	const bool go_through_errors_;
+	const JSON &root_, &root_schema_;
+	class Ref_ {
+		const JSON * ptr_;
+		std::string position_;
+	public:
+		explicit Ref_(const JSON &);
+		explicit Ref_(const Ref_ &);
+		explicit Ref_(const JSON &, const std::string &);
+		explicit Ref_(const Ref_ & r, const std::string &);
+		explicit Ref_(const Ref_ & r, const int &);
+		void step_(const std::string &pos);
+		void step_(const int &i);
+		void forward_(const std::string &pos);
+		const std::string & get_position_() const;
+		const JSON & get_json_() const;
+	};
+	// static std::map<const std::string, const std::string> format_;
+	std::stringstream & error_();
+	std::stringstream & error_(const Ref_ &);
+	void error_(const std::string &);
+	void error_(const Ref_ &, const std::string &);
+	Ref_ get_referece_(const JSON &, const std::string &);
+	void validate_array_(const Ref_ &, const JSON &);
+	void validate_object_(const Ref_ &, const JSON &);
+	void validate_string_(const Ref_ &, const JSON &);
+	void validate_number_(const Ref_ &, const JSON &);
+	void validate_integer_(const Ref_ &, const JSON &);
+	void validate_boolean_(const Ref_ &);
+	void validate_enum_(const Ref_ &, const JSON &);
+	bool validate_regex_(const std::string &, const std::string &);
+	bool validate_all_of_(const Ref_ &, const JSON &);
+	bool validate_any_of_(const Ref_ &, const JSON &);
+	bool validate_one_of_(const Ref_ &, const JSON &);
+	bool validate_not_(const Ref_ &, const JSON &);
+	void validate_(const Ref_ &, const JSON &, bool = true);
+	Json_Schema_Validator(const JSON & json, const Ref_ &, const JSON &schema,
+			bool = false);
+public:
+	Json_Schema_Validator(const JSON & json, const JSON & schema, bool = false);
+	bool get_result() const;
+	std::string get_error() const;
 };
 
-
-class JsonSchemaValidator {
-private:
-	std::stringstream _error;
-	bool _result;
-	const bool _goThroughErrors;
-	const Json::Value &_root, &_rootSchema;
-	static std::map<const std::string, const std::string> _format;
-	std::stringstream & error();
-	Reference getReferece(const Json::Value &, const std::string &);
-	void validateArray(const Reference &, const Json::Value &);
-	void validateObject(const Reference &, const Json::Value &);
-	void validateString(const Reference &, const Json::Value &);
-	void validateNumber(const Reference &, const Json::Value &);
-	void validateInteger(const Reference &, const Json::Value &);
-	void validateBoolean(const Reference &);
-	void validateEnum(const Reference &, const Json::Value &);
-	bool validateRegex(const std::string &, const std::string &);
-	bool validateAllOf(const Reference &, const Json::Value &);
-	bool validateAnyOf(const Reference &, const Json::Value &);
-	bool validateOneOf(const Reference &, const Json::Value &);
-	bool validateNot(const Reference &, const Json::Value &);
-	void validate(const Reference &, const Json::Value &, bool = true);
-	JsonSchemaValidator(const Json::Value &, const Reference &, const Json::Value &schema, bool = false);
-public:
-	JsonSchemaValidator(const Json::Value &, const Json::Value &, bool = false);
-	bool getResult() const;
-	std::string getError() const;
-};
-
-#endif /* JSONSCHEMAVALIDATOR_HH_ */
+#endif /* JSON_SCHEMA_VALIDATOR_HH_ */
