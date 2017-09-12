cfac12e40c48a4dbe7de5c42d36422b8cf1b40a7
Tibor Dravecz <t.gthb+17@integrity.hu>
2017-09-12 11:10:33 +0200

    Update helper.hpp
    some funcions moved to schema_validator.cc

--- helper.hpp
+++ helper.hpp
@@ -4,30 +4,6 @@
 #include "schema_validator.hh"
 typedef Json_Schema_Validator::JSON JSON;
 
-namespace algorithm {
-
-template<typename Tp_>
-struct LESS {
-	static bool compare(const Tp_ &a, const Tp_ &b) {
-		return a <= b;
-	}
-	static bool compare_exclusive(const Tp_ &a, const Tp_ &b) {
-		return a < b;
-	}
-};
-
-template<typename Tp_>
-struct MORE {
-	static bool compare(const Tp_ &a, const Tp_ &b) {
-		return a >= b;
-	}
-	static bool compare_exclusive(const Tp_ &a, const Tp_ &b) {
-		return a > b;
-	}
-};
-
-}
-
 namespace get {
 bool is_as_bool(const JSON & val) {
 	return val.isBool() && val.asBool();
@@ -62,33 +38,6 @@ template<>
 double as<double>(const JSON & val) {
 	return val.asDouble();
 }
-}
-
-template<typename Array_, typename Element_>
-decltype(auto) find_element(const Array_ & array, const Element_ & element) {
-	return std::find(array.begin(), array.end(), element);
-}
-
-template<typename Array_, typename Element_>
-bool find_bool(const Array_ & array, const Element_ & element) {
-	return find_element(array, element) != array.end();
-}
-
-template<template<typename > class alg_, typename Tp_>
-bool test_value(Tp_ test_value, const JSON &schema, const std::string & get,
-		const std::string & exclusive) {
-	if (get::is<Tp_>(schema[get])) {
-		auto max = get::as<Tp_>(schema[get]);
-		if (get::is_as_bool(schema[exclusive])) {
-			if (alg_<Tp_>::compare(test_value, max)) {
-				return true;
-			}
-		}
-		if (alg_<Tp_>::compare_exclusive(test_value, max)) {
-			return true;
-		}
-	}
-	return false;
 }
 
 #endif /* JSON_HELPER_HH_ */
